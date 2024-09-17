6b249520f6d214f5a87f4c9cdeff1845b6d3d247
Canyon Bryson <canyonbryson@gmail.com>
2024-09-17 16:03:10 -0600

    maybe

--- app.py
+++ app.py
@@ -41,35 +41,7 @@ itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 prediction = '\n'
-# Train and test splits
-# data = torch.tensor(encode(text), dtype=torch.long)
-# n = int(0.9*len(data)) # first 90% will be train, rest val
-# train_data = data[:n]
-# val_data = data[n:]
 
-# data loading
-# def get_batch(split):
-#     # generate a small batch of data of inputs x and targets y
-#     data = train_data if split == 'train' else val_data
-#     ix = torch.randint(len(data) - block_size, (batch_size,))
-#     x = torch.stack([data[i:i+block_size] for i in ix])
-#     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-#     x, y = x.to(device), y.to(device)
-#     return x, y
-
-# @torch.no_grad() # no back propogation
-# def estimate_loss():
-#     out = {}
-#     model.eval()
-#     for split in ['train', 'val']:
-#         losses = torch.zeros(eval_iters)
-#         for k in range(eval_iters):
-#             X, Y = get_batch(split)
-#             logits, loss = model(X, Y)
-#             losses[k] = loss.item()
-#         out[split] = losses.mean()
-#     model.train()
-#     return out
 
 class Head(nn.Module):
     """ one head of self-attention """
